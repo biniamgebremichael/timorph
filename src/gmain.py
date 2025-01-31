534139acf9923f84e9a88d43aa9b1b41de3c56ee
Biniam <bin_mil@yahoo.com>
2025-01-31 13:41:41 -0500

    add src

--- src/gmain.py
+++ src/gmain.py
@@ -21,14 +21,15 @@ def runner(form, root):
     # store(root, json.dumps({"map":map}))
 
 
-def runOnBaseTense(parentroot,short_path,long_path, word, pos, base_tense, forms):
+def runOnBaseTense(parentroot, short_path, long_path, word, pos, base_tense, forms):
     dbPersistor = DbPersistor()
     if (base_tense in forms[pos]):
         for feature in forms[pos][base_tense]:
             if (dbPersistor.existGermination(word, pos, feature) <= 0):
                 map = runner(feature, word)
-                pos_output = 'N' if feature=='VERB2NOUN' else pos
-                map_ = [x.to_tuple() for x in Germination.objectify(parentroot,short_path,long_path, word,  pos_output, feature, map)]
+                pos_output = 'N' if feature == 'VERB2NOUN' else pos
+                map_ = [x.to_tuple() for x in
+                        Germination.objectify(parentroot, short_path, long_path, word, pos_output, feature, map)]
                 dbPersistor.addGermination(map_)
                 csvPrint2(word, short_path + ">>" + feature, map)
             else:
@@ -39,9 +40,10 @@ def run(germs):
     futures = {}
     with ProcessPoolExecutor(max_workers=5) as executor:
         for germ in germs:
-            short_path = germ.feature if germ.feature=="ROOT" else germ.shortpath + '.' + germ.feature
-            long_path = germ.feature if germ.feature=="ROOT" else germ.longpath + '.' + germ.feature + '-' + germ.subject + '+' + germ.object
-            futures[executor.submit(runOnBaseTense, germ.parent, short_path,long_path, germ.germinated, germ.pos, germ.feature,
+            short_path = germ.feature if germ.feature == "ROOT" else germ.shortpath + '.' + germ.feature
+            long_path = germ.feature if germ.feature == "ROOT" else germ.longpath + '.' + germ.feature + '-' + germ.subject + '+' + germ.object
+            futures[executor.submit(runOnBaseTense, germ.parent, short_path, long_path, germ.germinated, germ.pos,
+                                    germ.feature,
                                     forms)] = germ
     for future in as_completed(futures):
         future.result()
@@ -51,24 +53,25 @@ if __name__ == '__main__':
 
     fst = FstMap()
 
-    #cecece = [Germination.rootGermination("ሓተተ","V")]
-    nouns = [Germination.rootGermination(x[0],x[1]) for x in getNouns()]
-    verbs = [Germination.rootGermination(x[0],x[1]) for x in getNouns()]
-    cecece = verbs+nouns
+    # cecece = [Germination.rootGermination("ሓተተ","V")]
+    nouns = [Germination.rootGermination(x[0], x[1]) for x in getNouns()]
+    verbs = [Germination.rootGermination(x[0], x[1]) for x in getNouns()]
+    cecece = verbs + nouns
     forms = {
         "V":
             {
-                "ROOT": ["PAST", "PRESENT", "VERBPREFIXPAST", "VERBSUFFIX", "PASSIVE", "VERB2NOUN","VERBY"],
-                "PAST": ["VERBPREFIXPAST", "VERBSUFFIX" ],
-                "VERBY": ["VERBPREFIXPAST", "VERBSUFFIX" ],
-                "PRESENT": ["VERBPREFIXPRESENT", "VERBSUFFIX" ],
+                "ROOT": ["PAST", "PRESENT", "VERBPREFIXPAST", "VERBSUFFIX", "PASSIVE", "VERB2NOUN", "VERBY"],
+                "PAST": ["VERBPREFIXPAST", "VERBSUFFIX"],
+                "VERBY": ["VERBPREFIXPAST", "VERBSUFFIX"],
+                "PRESENT": ["VERBPREFIXPRESENT", "VERBSUFFIX"],
                 "VERB2NOUN": ["POSSESSIVE", "NOUNSUFFIX", "NOUNPREFIX"],
-                #"VERBPREFIXPAST": ["VERBSUFFIX"],
-                #"VERBPREFIXPRESENT": ["VERBSUFFIX"],
+                # "VERBPREFIXPAST": ["VERBSUFFIX"],
+                # "VERBPREFIXPRESENT": ["VERBSUFFIX"],
             },
         "N":
-            {"ROOT": ["POSSESSIVE", "NOUNPLURAL", "NOUNSUFFIX", "NOUNPREFIX"],
-             "POSSESSIVE": [  "NOUNSUFFIX", "NOUNPREFIX"],
+            {"ROOT": ["POSSESSIVE", "NOUNPLURAL", "NOUNPREFIX"],
+             "POSSESSIVE": ["NOUNPLURAL", "NOUNPREFIX"],
+             "NOUNPREFIX": ["NOUNSUFFIX"],
              },
         "A": {"ROOT": ["ADJPLURAL", "ADJPREFIX"]}
     }
@@ -79,4 +82,4 @@ if __name__ == '__main__':
         run(dbPersistor.getGermination(c.parent))
 
     time_start_time = time.time() - start_time
-    print("Elapsed time ", time_start_time, 'sec - ', time_start_time/60, 'min - ', time_start_time/3600, 'hrs - ')
+    print("Elapsed time ", time_start_time, 'sec - ', time_start_time / 60, 'min - ', time_start_time / 3600, 'hrs - ')
